db499284bca90622ab2c5d3985d1a8415b0b0969
Joseba <joseba@debian.localhost>
2026-01-21 18:47:45 +0100

    fixed

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -1,4 +1,3 @@
-// src/middleware.ts
 import type { MiddlewareHandler } from 'astro';
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
@@ -8,31 +7,16 @@ export const onRequest: MiddlewareHandler = async (context, next) => {
 		'Content-Security-Policy',
 		[
 			"default-src 'self';",
-
-			// Scripts locales, Ahrefs, Google reCAPTCHA
-			"script-src 'self' https://analytics.ahrefs.com https://www.google.com https://www.gstatic.com https://www.recaptcha.net 'unsafe-inline';",
-
-			// Estilos (permite inline por compatibilidad)
+			"script-src 'self' https://www.google.com https://www.gstatic.com https://www.recaptcha.net https://www.googletagmanager.com 'unsafe-inline';",
 			"style-src 'self' 'unsafe-inline';",
-
-			// Imágenes locales, Cloudinary, Google
-			"img-src 'self' https://res.cloudinary.com https://www.gstatic.com https://www.google.com data:;",
-
-			// Fuentes locales
+			"img-src 'self' https://res.cloudinary.com https://www.gstatic.com https://www.google.com https://www.google-analytics.com data:;",
 			"font-src 'self';",
-
-			// Conexiones externas (API, reCAPTCHA, Ahrefs)
-			"connect-src 'self' https://www.google.com https://www.recaptcha.net https://www.gstatic.com https://analytics.ahrefs.com;",
-
-			// iframes de reCAPTCHA
+			"connect-src 'self' https://www.google.com https://www.recaptcha.net https://www.gstatic.com https://www.google-analytics.com;",
 			'frame-src https://www.google.com https://www.recaptcha.net;',
-
-			// Opcional: bloquear objetos
 			"object-src 'none';",
 		].join(' '),
 	);
 
-	// Otros headers de seguridad
 	response.headers.set('X-Frame-Options', 'DENY');
 	response.headers.set('X-XSS-Protection', '1; mode=block');
 	response.headers.set('X-Content-Type-Options', 'nosniff');
